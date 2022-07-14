fafaa1c3f0aef4c88d7cacbbb7a4540774eb5b81
Khambazarov <khambazarovrenat@gmail.com>
2022-07-14 17:59:29 +0200

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ButtonMUI } from "../materialUI/ButtonMUI";
-// import { LinkHome, LinkProjects, LinkCV, LinkContact } from "./Links";
 
 export const Home = () => {
   return (
